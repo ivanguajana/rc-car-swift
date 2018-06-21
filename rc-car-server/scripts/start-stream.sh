6bf6473707e6e3262994d0b92abf768b5ad31bfb
Ivan Guajana <ivan.guajana@vilea.ch>
2018-06-21 12:01:43 +0100

    fixed line endings

--- rc-car-server/scripts/start-stream.sh
+++ rc-car-server/scripts/start-stream.sh
@@ -1,6 +1,3 @@
 #! /bin/bash
 
-uv4l -nopreview --auto-video_nr --driver raspicam --encoding mjpeg --width 640 -
--height 480 --framerate 20 --rotation 180 --server-option --port=9090 --server-o
-ption --max-queued-connections=30 --server-option --max-streams=25 --server-opti
-on --max-threads=29
+uv4l -nopreview --auto-video_nr --driver raspicam --encoding mjpeg --width 640 --height 480 --framerate 20 --rotation 180 --server-option --port=9090 --server-option --max-queued-connections=30 --server-option --max-streams=25 --server-option --max-threads=29
